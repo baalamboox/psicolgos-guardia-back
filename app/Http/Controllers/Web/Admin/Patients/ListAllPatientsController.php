d825a0bb4d58da4f4f9c7d2186d0a492dd080b85
Ferpeke <ferna9752@gmail.com>
2024-04-16 14:01:11 -0600

    Controlador de lista de pacientes: Creada

---
+++ app/Http/Controllers/Web/Admin/Patients/ListAllPatientsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Web\Admin\Patients;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class ListAllPatientsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
