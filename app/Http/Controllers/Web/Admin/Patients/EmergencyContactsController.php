5b4e3d96dc12ccc4f1b9250073e163d256a44849
Ferpeke <ferna9752@gmail.com>
2024-04-18 13:02:00 -0600

    Sección de Contactos-Pacientes: Terminada

--- app/Http/Controllers/Web/Admin/Patients/EmergencyContactsController.php
+++ app/Http/Controllers/Web/Admin/Patients/EmergencyContactsController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers\Web\Admin\Patients;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class EmergencyContactsController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(int $id)
     {
-        
+        $listAllEmergencyContacts = User::where('id', $id)->where('profile_id', 2)->with('emergencyContact')->first();
+        if($listAllEmergencyContacts != null)
+        {
+            return response()->json($listAllEmergencyContacts, 200);
+        }else{
+            return response()->json('No corresponde a un paciente', 400);
+        }
     }
 }
